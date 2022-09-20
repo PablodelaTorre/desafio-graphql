44e238741be089ac9b221fac2768ae8139f2c02e
Pablo <delatorre.pablodaniel@gmail.com>
2022-09-19 23:46:10 -0300

    avances con schema y resolvers

--- src/server.js
+++ src/server.js
@@ -1,7 +1,19 @@
 import express from "express";
 import { graphqlHTTP } from 'express-graphql'
+import {makeExecutableSchema} from '@graphql-tools/schema'
+import {typeDefs} from 'schema.js'
+import {resolvers} from 'resolvers.js'
+
+const schema = makeExecutableSchema({
+    typeDefs:typeDefs,
+    resolvers:resolvers
+})
+
 const app = express()
 
+app.use('/graphql',graphqlHTTP({
+    schema:schema
+}))
 
 const PORT = process.env.PORT || 3000
 app.listen(PORT,()=> {
